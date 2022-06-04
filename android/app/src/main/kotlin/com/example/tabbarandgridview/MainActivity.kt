cfed29f2e9007d61f682f29cd5689cda0b887565
Timur <timur.krasavchik.91@mail.ru>
2022-06-04 21:19:49 +0500

    Product cart

---
+++ android/app/src/main/kotlin/com/example/tabbarandgridview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tabbarandgridview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:tabbarandgridview/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        theme: ThemeData(primaryColor: Colors.white),
+        home:Home_Screen()
+    );
+
+  }
+}
